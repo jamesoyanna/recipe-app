5e3490a82c0159e9cabcc8d96de49b5690e8f0b5
eshoiza <engrjayt200@gmail.com>
2019-12-03 18:56:28 +0100

    added css mosule

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import style from './recipe.module.css';
+
 
 const Recipe = ({title, calories, ingredients, image }) => {
   return (
-    <div>
+    <div className={ style.recipe}>
       <h1>{title}</h1>
       <ol>
         {ingredients.map(ingredients=>(
